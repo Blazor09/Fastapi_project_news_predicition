2ce5f5db5077bc4854c993de973dbfdd43e300a4
Blazor09 <99520032+Blazor09@users.noreply.github.com>
2026-04-27 14:40:20 +0545

    Delete models.py
    Looking at my current main.py, i am defining my posts as a local list. If models.py is an old leftover from a previous version and isn't being imported anywhere, delete it to avoid confusion.

--- models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class Post(BaseModel):
-    id: int
-    author: str
-    title: str
-    content: str
-    date_posted: str = datetime.now().strftime("%B %d, %Y")
